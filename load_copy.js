e6c96dd28d95a805152914ee300138efc3dd35fd
Ryan Ideh <idehryan@gmail.com>
2024-07-03 21:48:46 +0100

    Base

---
+++ load_copy.js
@@ -0,0 +1,45 @@
+const ul = document.getElementById('apidata');
+const list = document.createDocumentFragment();
+
+function loadDetails () {
+
+    userID = document.getElementById("userSearch").value;
+    //debugger
+    url = 'https://overfast-api.tekrop.fr/players/' + userID + '/summary';
+
+    fetch(url)
+    .then(response => response.json())
+    .then((data) => {
+        //let apidata = data;
+        console.log(data);
+        document.getElementById("usernameInfo").innerHTML = data.username;
+        document.getElementById("title").innerHTML = data.title;
+        document.getElementById("endorsement").innerHTML = data.endorsement.level;
+        document.getElementById("endFrame").src = data.endorsement.frame;
+        document.getElementById("avatar").src = data.avatar;
+        document.getElementById("namecard").src = data.namecard;
+
+        });
+
+    }
+
+document.getElementById("submit").addEventListener("click", loadDetails);
+
+
+
+    //apidata.map(function(info) {
+    //    let li = document.createElement('li');
+    //    let username = document.createElement('h2');
+     //   let title = document.createElement('span');
+
+    //    username.innerHTML = `${apidata.username}`;
+    //    title.innerHTML = `${apidata.title}`;
+    //});
+
+   //})
+
+   //ul.appendChild(list);
+
+
+// Second API Call for QP Stats: https://overfast-api.tekrop.fr/players/Raxey-21971/stats/summary?gamemode=quickplay&platform=pc
+
