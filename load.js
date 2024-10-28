ca4227fbc2233ed745dba7a8da6e48ae1ade360f
Ryan Ideh <idehryan@gmail.com>
2024-10-28 12:31:50 +0000

    Update

--- load.js
+++ load.js
@@ -1,19 +1,20 @@
-const ul = document.getElementById('apidata');
-const list = document.createDocumentFragment();
 
 function loadDetails () {
 
-    userID = document.getElementById("userSearch").value;
     //userID = "Raxey-21971";
-    url = 'https://overfast-api.tekrop.fr/players/'+ userID +'/summary';
+    const userID = document.getElementById("userSearch").value;
+    const infoloadDiv = document.getElementById("infoload");
+    const url = 'https://overfast-api.tekrop.fr/players/'+ userID +'/summary';
+
+    infoloadDiv.style.display = 'flex';
 
     fetch(url)
     .then(response => response.json())
     .then((data) => {
-        console.log(data);
+        
         document.getElementById("usernameInfo").innerHTML = data.username;
         document.getElementById("title").innerHTML = data.title;
-        document.getElementById("endorsement").innerHTML = data.endorsement.level;
+        //document.getElementById("endorsement").innerHTML = data.endorsement.level;
         document.getElementById("endFrame").src = data.endorsement.frame;
         document.getElementById("avatar").src = data.avatar;
         document.getElementById("namecard").src = data.namecard;
@@ -24,22 +25,6 @@ function loadDetails () {
 window.addEventListener("DOMContentLoaded", (event) => {
     document.getElementById("submit").addEventListener("click", loadDetails);
 });
-
-
-
-    //apidata.map(function(info) {
-    //    let li = document.createElement('li');
-    //    let username = document.createElement('h2');
-     //   let title = document.createElement('span');
-
-    //    username.innerHTML = `${apidata.username}`;
-    //    title.innerHTML = `${apidata.title}`;
-    //});
-
-   //})
-
-   //ul.appendChild(list);
-
 
 // Second API Call for QP Stats: https://overfast-api.tekrop.fr/players/Raxey-21971/stats/summary?gamemode=quickplay&platform=pc
 
